6e340f3f9e59e339ba5c921312b5804c804253cd
Денис <obogrev.help@gmail.com>
2020-10-22 02:14:04 +0300

    добавляет задание 2

--- js/task-2.js
+++ js/task-2.js
@@ -7,24 +7,13 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
+const toggleUserState = (allUsers, userName) => {
+  return Promise.resolve(allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
-  );
-
-  callback(updatedUsers);
+  ));
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
