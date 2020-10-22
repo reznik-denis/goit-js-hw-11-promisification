d061106e0d1aff67a231d8331b366237892c0648
Денис <obogrev.help@gmail.com>
2020-10-22 03:33:16 +0300

    добавляет задание 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,53 +1,43 @@
 // Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
 
-const randomIntegerFromInterval = (min, max) => {
+const makeTransaction = (transaction) => {
+  const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((resolve, reject) => {
+    const canProcess = Math.random() > 0.3;
+    setTimeout(() => {
+      const id = transaction.id;
+      if (canProcess) {
+        resolve({ id, delay });
+      } 
+        reject(id);
+    }, delay);
+  });
+};
+
+function randomIntegerFromInterval (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
-  const delay = randomIntegerFromInterval(200, 500);
-
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
-};
-
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, delay }) => {
+  console.log(`Transaction ${id} processed in ${delay}ms`);
 };
 
 const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
-makeTransaction({ id: 70, amount: 150 })
+makeTransaction({ id: 70})
   .then(logSuccess)
   .catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
+makeTransaction({ id: 71})
   .then(logSuccess)
   .catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
+makeTransaction({ id: 72})
   .then(logSuccess)
   .catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
+makeTransaction({ id: 73})
   .then(logSuccess)
   .catch(logError);
